aff09b81e32f09ba3b001043f326ef27acec49c9
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-09-15 19:34:09 +0200

    Hand and deck made

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Deck : MonoBehaviour {
+
+    [SerializeField]
+    List<TileType> cardType;
+
+    [SerializeField]
+    List<int> initialCount;
+
+    List<Tile> deck = new List<Tile>();
+
+    [SerializeField]
+    Tile prefab;
+
+    int nextCard = 0;
+
+	void Start () {
+        CreateCards();
+        ShuffleCards();
+	}
+	
+    void CreateCards()
+    {
+        if (deck.Count > 0)
+        {
+            return;
+        }
+
+        for (int i=0; i<cardType.Count; i++)
+        {
+            for (int j=0; j<initialCount[i]; j++)
+            {
+                Tile t = Instantiate(prefab);
+                t.SetType(cardType[i]);
+                t.transform.SetParent(transform);
+                t.gameObject.SetActive(false);
+                deck.Add(t);
+            }
+
+        }
+
+    }
+
+    void ShuffleCards()
+    {
+        int n = deck.Count;
+        int l = n;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, l);
+            Tile value = deck[k];
+            deck[k] = deck[n];
+            deck[n] = value;
+        }
+    }
+
+    public Tile GetCard()
+    {
+        CreateCards();
+        int n = deck.Count;
+
+        if (n == 0)
+        {
+            return null;
+        }
+        if (nextCard >= n)
+        {
+            nextCard %= n;
+            ShuffleCards();
+        }
+        Tile t = deck[nextCard];
+        nextCard++;
+        return t;
+    }
+}
---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Hand : MonoBehaviour {
+
+    Plane plane = new Plane(Vector3.forward, Vector3.up);
+
+    [SerializeField, Range(0, 1)]
+    float xPostion;
+
+    [SerializeField, Range(0, 1)]
+    float bottomOffset;
+
+    [SerializeField, Range(1, 10)]
+    int handSize;
+
+    [SerializeField, Range(0, 1)]
+    float width;
+
+    List<Tile> tilesInHand = new List<Tile>();
+
+    [SerializeField]
+    Deck deck;
+
+    Vector3 xMin;
+    Vector3 xMax;
+
+    void Start()
+    {
+        PositionOnScreen();
+
+        for (int i = 0; i< handSize; i++)
+        {
+            Tile t = deck.GetCard();
+            float f = i / (handSize - 1.0f);
+            t.transform.position = Vector3.Lerp(xMin, xMax, f);
+            t.transform.SetParent(transform, true);
+            t.gameObject.SetActive(true);
+        }
+
+    }
+
+
+    void LateUpdate()
+    {
+        PositionOnScreen();
+    }
+
+    void PositionOnScreen()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width * xPostion, Screen.height * bottomOffset));
+        float distance;
+        if (plane.Raycast(ray, out distance))
+        {
+            transform.position = ray.GetPoint(distance);
+        }
+
+        ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width * (xPostion - width / 2), Screen.height * bottomOffset));        
+        if (plane.Raycast(ray, out distance))
+        {
+            xMin = ray.GetPoint(distance);
+        }
+
+        ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width * (xPostion + width / 2), Screen.height * bottomOffset));
+        if (plane.Raycast(ray, out distance))
+        {
+            xMax = ray.GetPoint(distance);
+        }
+
+        for (int i=0, l=transform.childCount; i< l; i++)
+        {
+            float f = i / (handSize - 1.0f);
+            transform.GetChild(i).position = Vector3.Lerp(xMin, xMax, f);
+        }
+
+    }
+}
